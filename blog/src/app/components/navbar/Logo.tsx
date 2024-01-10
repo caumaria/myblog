361e78c63704c286275c1ca245cfe71cdfb1293a
Cau <cau.ugolini@gmail.com>
2024-01-10 16:16:31 -0300

    style<Navbar>: add hover animation + logo link

--- blog/src/app/components/navbar/Logo.tsx
+++ blog/src/app/components/navbar/Logo.tsx
@@ -1,16 +1,20 @@
+import Link from "next/link";
 import React from "react";
 import styled from "styled-components";
 
 const LogoContainer = styled.div`
   color: var(--lettering);
   font-family: "Cedarville Cursive", cursive;
-  font-size: 24px;
+  font-size: 2.2rem;
+  a {
+    text-decoration: none;
+  }
 `;
 
 function Logo() {
   return (
     <LogoContainer>
-      <h1>pitada de sal</h1>
+      <Link href="/">pitada de sal</Link>
     </LogoContainer>
   );
 }
--- blog/src/app/components/navbar/NavLinks.tsx
+++ blog/src/app/components/navbar/NavLinks.tsx
@@ -3,31 +3,51 @@ import styled from "styled-components";
 import Link from "next/link";
 
 const Ul = styled.ul`
-    display: flex;
-    gap: 2rem;
-    a {
-        margin-top: 10px;
-        text-decoration: none;
-        color: var(--title);
-        text-transform: uppercase;
-        font-size: 1.2rem;
-        font-family: "Courier New", monospace;   
-        
-        img {
-          width: 20px;
-        }
+  display: flex;
+  gap: 2rem;
+  a {
+    position: relative;
+    margin-top: 10px;
+    text-decoration: none;
+    color: var(--title);
+    text-transform: uppercase;
+    font-size: 1.3rem;
+    font-family: "Courier New", monospace;
+    img {
+      width: 20px;
     }
+    &:hover {
+      color: var(--lettering);
+    }
+  }
+  a::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 3px;
+    border-radius: 4px;
+    background-color: var(--lettering);
+    bottom: 0;
+    left: 0;
+    transform-origin: right;
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+  }
+  a:hover::before {
+    transform-origin: left;
+    transform: scaleX(1);
+  }
 `;
 
 export default function NavLinks() {
   return (
     <Ul>
-        <Link href='/'>Home</Link>
-        <Link href='/recepies'>Receitas</Link>
-        <Link href='/about'>Sobre</Link>
-        <Link href='/search'><img src="/lupa.png" alt="ícone de lupa para pesquisar receitas" /></Link>
-
-
+      <Link href="/">Home</Link>
+      <Link href="/recepies">Receitas</Link>
+      <Link href="/about">Sobre</Link>
+      <Link href="/search">
+        <img src="/lupa.png" alt="ícone de lupa para pesquisar receitas" />
+      </Link>
     </Ul>
   );
 }
